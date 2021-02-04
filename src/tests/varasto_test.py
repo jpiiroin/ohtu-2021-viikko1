c5ceb627206a7c871bfa5278d99220db55e40e10
Janne Piiroinen <jpiiroin@hotmail.com>
2021-02-04 17:50:50 +0200

    coveragesysteemit

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,10 +5,21 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        self.varastokaksi = Varasto(-1, -1)
+        self.varastokolme = Varasto(10,30)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_tilavuus_ei_voi_olla_neg(self):
+        self.assertAlmostEqual(self.varastokaksi.saldo, 0)
+
+    def test_alkusaldo_ei_voi_olla_neg(self):
+        self.assertAlmostEqual(self.varastokaksi.saldo, 0)
+
+    def test_ylimaara_konstruktorissa(self):
+        self.assertAlmostEqual(self.varastokolme.saldo, 10)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
@@ -38,3 +49,29 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ei_voi_ottaa_neg(self):
+        maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(maara, 0)
+
+    def test_ei_voi_ottaa_liikaa(self):
+        self.varasto.lisaa_varastoon(6)
+        maara = self.varasto.ota_varastosta(30)
+        self.assertAlmostEqual(maara, 6)
+
+    def test_ei_voi_lisata_neg(self):
+        self.varasto.lisaa_varastoon(1)
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 1)
+
+    def test_ei_voi_lisata_liikaa(self):
+        self.varasto.lisaa_varastoon(30)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_printtaus_oikein(self):
+        tuloste = str(self.varasto)
+        print(tuloste)
+        self.assertAlmostEqual(tuloste, "saldo = 0, vielä tilaa 10")
+
+
+        
